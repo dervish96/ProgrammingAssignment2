4b3f34a8be22633f2c7d50a8a2fca720273d3885
dervish96 <kkdervesh@yahoo.com>
2017-08-12 22:15:31 -0500

    Cache the Inverse
    Change the forked file and add the necessary code changes to compute
    inverse of a matrix and then place it in a cache. Subsequently retrieve
    it from a Cache if there, otherwise compute fresh inverse

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+ ## These two functions create an inverse of a matrix and places it in a cache.
+## If a new matrix is passed to makeCacheMatrix, then a fresh inverse is calculated 
 
-## Write a short comment describing this function
+## This function places the inverse of a function in the Cache
 
 makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(Inverse) m <<- Inverse
+  getInverse <- function() m
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of a matrix if it is not already in the Cache
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getInverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setInverse(m)
+  
+  m
+  
+  
 }
